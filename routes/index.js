918348d01e1784cc3b4335a6ab8d89085160f497
= <=>
2017-10-01 09:51:34 +0800

    第一次提交，辛泽晨创建的头部和底部

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
+
+// 加载模块 
 var express = require('express');
 var router = express.Router();
 
+// 加载对应的控制器
+var index = require('../controllers/index')
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', index.index);
 
 module.exports = router;
